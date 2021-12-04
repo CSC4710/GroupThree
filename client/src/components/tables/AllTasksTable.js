46cb49a4a9e062a91099f6b4a10c80e38ac60072
Chris Gumieny <gr4338@wayne.edu>
2021-12-04 02:06:30 -0500

    Adding edit task functionality

--- client/src/components/tables/AllTasksTable.js
+++ client/src/components/tables/AllTasksTable.js
@@ -11,7 +11,10 @@ import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Button from '@mui/material/Button';
 import axios from "axios";
+import Axios from "axios";
 
+// UpdateTaskForm
+import UpdateTask from "../UpdateTask";
 
 
 // Create columns for id, description, due date, priority, category, status and actions
@@ -52,6 +55,73 @@ export default function StickyHeadTable() {
 
     const [TasksList, setTasksList] = useState([]);
 
+    //Update Task
+    const [updateTaskForm, setUpdateTaskForm] = useState({ isOpen: false, message: "", type: "" });
+
+    const handleOpen = (description, date, priority, categories, status) => {
+        getCategories();
+        setTasks_description(description);
+        setTasks_due_date(date);
+        setTasks_priority(priority);
+        setTasks_categories(categories);
+        setTasks_status(status);
+
+        if (priority === "Priority 2")
+            setOldPriority(2);
+        else if (priority === "Priority 3")
+            setOldPriority(3);
+        else if (priority === "Priority 4")
+            setOldPriority(4);
+        else
+            setOldPriority(1);
+
+        if (status === "Active")
+        {
+            setActiveButton("contained");
+            setCompleteButton("outlined");
+        }
+        else if (status === "Completed")
+        {
+            setActiveButton("outlined");
+            setCompleteButton("contained");
+        }
+        else
+        {
+            setActiveButton("outlined");
+            setCompleteButton("outlined");
+        }
+        handleUpdateTaskForm();
+    }
+
+    const handleUpdateTaskForm = () => {
+        setUpdateTaskForm({ isOpen: true, type: "success" });
+    };
+
+    const [tasks_id_to_change, setTasks_id_to_change] = React.useState("");
+    const [tasks_description, setTasks_description] = React.useState("");
+    const [tasks_due_date, setTasks_due_date] = React.useState(null);
+    const [tasks_priority, setTasks_priority] = React.useState('');
+    const [tasks_categories, setTasks_categories] = React.useState('');
+    const [tasks_status, setTasks_status] = React.useState('');
+
+    // Function to update a task
+    const updateTask = (tasks_description, tasks_due_date, tasks_priority, tasks_categories, tasks_status) => {
+        axios.put(`https://csc4710dbs.herokuapp.com/api/updateTask/`, {
+            Tasks_id: tasks_id_to_change,
+            tasks_description: tasks_description,
+            tasks_categories: tasks_categories,
+            tasks_priority: tasks_priority,
+            tasks_status: tasks_status,
+            tasks_due_date: tasks_due_date
+        }).then(res => {
+            console.log("Task updated");
+            setRows(rows.map(row => (row.Tasks_id === tasks_id_to_change ? { Description: tasks_description } : row)));
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
 
     // Function to delete a task
     const deleteTask = (Tasks_id) => {
@@ -65,6 +135,26 @@ export default function StickyHeadTable() {
             window.location.reload();
         });
     }
+
+    // Function to call https://csc4710dbs.herokuapp.com/api/getCategories and map through the tasks_categories and make a dropdown menu
+    const [categories, setCategories] = useState([]);
+    const getCategories = () => {
+        Axios.get("https://csc4710dbs.herokuapp.com/api/getCategories").then(res => {
+            setCategories(res.data);
+        }).then(() => {
+            console.log(categories);
+        }
+        )
+        .catch(err => {
+            console.log(err);
+        }
+        )
+    }
+    
+    // useStates in order to dynamically change the button
+    const [completeButton, setCompleteButton] = React.useState('');
+    const [activeButton, setActiveButton] = React.useState('');
+    const [oldPriority, setOldPriority] = React.useState('');
 
 
     return (
@@ -104,7 +194,13 @@ export default function StickyHeadTable() {
                                     <TableCell
                                         style={{ minWidth: 17, align: 'right', color: '#1972d8', size: 'x-small' }}
                                     >{row.Tasks_actions}
+                                    <Button onClick={() => {
+                                    handleOpen(row.tasks_description, row.tasks_due_date, 
+                                        row.tasks_priority, row.tasks_categories, row.tasks_status);
+                                    setTasks_id_to_change(row.Tasks_id);
+                                    }}>
                                         <EditIcon />
+                                    </Button>
                                     </TableCell>
 
 
@@ -135,6 +231,28 @@ export default function StickyHeadTable() {
                 onPageChange={handleChangePage}
                 onRowsPerPageChange={handleChangeRowsPerPage}
             />
+
+        <UpdateTask
+            updateTaskForm={updateTaskForm}
+            setUpdateTaskForm={setUpdateTaskForm}
+            categories={categories}
+            tasks_description={tasks_description}
+            setTasks_description={setTasks_description}
+            tasks_due_date={tasks_due_date}
+            setTasks_due_date={setTasks_due_date}
+            tasks_priority={tasks_priority}
+            setTasks_priority={setTasks_priority}
+            tasks_status={tasks_status}
+            setTasks_status={setTasks_status}
+            tasks_categories={tasks_categories}
+            setTasks_categories={setTasks_categories}
+            oldPriority={oldPriority}
+            completeButton={completeButton}
+            setCompleteButton={setCompleteButton}
+            activeButton={activeButton}
+            setActiveButton={setActiveButton}
+            updateTask = {updateTask}
+        />
         </Paper>
     );
 }
